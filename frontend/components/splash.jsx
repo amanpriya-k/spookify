0b37ab4ac90ecd59159ef0b1757a4a6a962fae24
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-10-10 15:23:55 -0700

    Fix assets for heroku

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -4,33 +4,55 @@ import LoginFormContainer from './session/login_form_container'
 import SignupFormContainer from './session/signup_form_container'
 import SideNav from './side_nav';
 
-const Splash = () => {
-  return (
-    <div className='splash'>
-        <div className='splash-nav'>
-          <a href="/#/" className='splash-logo'>
+class Splash extends React.Component {
+
+  componentDidMount() {
+    document.title = "Spookify"
+  }
+
+  render() {
+    return (
+      <div className='splash'>
+          <div className='splash-nav'>
+            <a href="/#/" className='splash-logo'>
+              <p>Spookify</p>
+              <img src='/assets/white-icon'></img>
+            </a>
+
+            <div className='splash-nav-links'>
+              <Link to="/signup"> Sign Up </Link>
+              <Link to="/login"> Login </Link>
+            </div>
+          </div>
+
+          <div className='splash-img'>
+            <h1>Music for everyone.</h1>
+            <h3>Millions of songs. No credit card needed.</h3>
+            <Link className='splash-btn' to="/signup"> GET SPOOKIFY FREE </Link>
+          </div>
+
+          <div className="splash-bottom">
             <p>Spookify</p>
             <img src='/assets/white-icon'></img>
-          </a>
-
-          <div className='splash-nav-links'>
-            <Link to="/signup"> Sign Up </Link>
-            <Link to="/login"> Login </Link>
+            <div className="bottom-lists">
+              <div>
+                <h3>About the developer</h3>
+                <h4><a href="/#/">GitHub </a> </h4>
+                <h4><a href="/#/">LinkedIn </a> </h4>
+                <h4><a href="/#/">Resume </a> </h4>
+              </div>
+              <div>
+                <h3>Useful Links</h3>
+                <h4><a href="/#/signup"> Sign Up </a> </h4>
+                <h4><a href="/#/login"> Log In </a> </h4>
+                <h4><a href="/#/"> Home </a> </h4>
+              </div>
+            </div>
           </div>
-        </div>
+      </div>
+    )
+  }
 
-        <div className='splash-img'>
-          <h1>Music for everyone.</h1>
-          <h3>Millions of songs. No credit card needed.</h3>
-          <Link className='splash-btn' to="/signup"> GET SPOOKIFY FREE </Link>
-        </div>
-
-        <div className="splash-bottom">
-          <p>Spookify</p>
-          <img src='/assets/white-icon'></img>
-        </div>
-    </div>
-  )
 }
 
 export default Splash;
