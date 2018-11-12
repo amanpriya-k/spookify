72475c51c5d4e992e0217f06fd42620708860c60
amanpriya-k <amanpriya.kulkarni@gmail.com>
2018-11-12 11:39:31 -0800

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# destroy everything in db
 
 User.destroy_all
 Song.destroy_all
--- frontend/components/main.jsx
+++ frontend/components/main.jsx
@@ -34,7 +34,6 @@ class Main extends React.Component {
 
     return(
       <div className="main">
-
         <div className="top-half">
           <Modal></Modal>
 
